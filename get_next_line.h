37ed9d7fe0a115f01bb63395a12b40fd444c7771
Omar AMAIRI <mounioma@gmail.com>
2025-05-08 02:18:20 +0200

    first push

---
+++ get_next_line.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oamairi <oamairi@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/07 00:47:15 by oamairi           #+#    #+#             */
+/*   Updated: 2025/05/08 01:30:30 by oamairi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# define BUFFER_SIZE 6
+
+char	*get_next_line(int fd);
+
+#endif
